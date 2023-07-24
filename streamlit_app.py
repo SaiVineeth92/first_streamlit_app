b8c4033451ef2a42f4899fa75f42a0d5ecefddc4
SaiVineeth92 <saivineeth05@gmail.com>
2023-07-24 15:52:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title("my parents new healthy diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 🥗 🐔 🥑🍞 Omega 3 and Bulberry oat meal")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
